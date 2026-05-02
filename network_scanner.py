184ea53cbbc07895e9f8117b3711b9c4cd739dd3
j037-8a8u <112858813+j037-8a8u@users.noreply.github.com>
2026-05-02 23:02:26 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,36 +1,36 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 
 import scapy.all as scapy
 import argparse
 
-
-def get_arguments():
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--ip", dest="ip", help="IP or range of IPs to scan for")
+    parser.add_argument("-i", "--ip", dest="ip", help="IP address or range to scan.")
     options = parser.parse_args()
-    if not options.ip:
-        parser.error("[-] Please enter an IP or a range of IPs. Use --help for more info.")
-    return options
+    if options.ip:
+        return options
+    else:
+        parser.error("No arguments specified. Use -h for help.")
 
-
-def scan(ip):
+def arp_scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
-    return clients_list
+    arp_request_frame = broadcast/arp_request
+    ans_list = scapy.srp(arp_request_frame, timeout=1, verbose=False)[0]
+    client_list = []
 
+    for element in ans_list:
+        client = {"ip":element[1].psrc, "mac":element[1].hwsrc}
+        client_list.append(client)
 
-def print_result(results_list):
-    print("IP\t\t\tMAC Address\n------------------------------------------")
-    for client in results_list:
-        print(client["ip"] + "\t\t" + client["mac"])
+    return client_list
 
+def print_results(client_list):
+    print("IPv4 address\t\t\tMAC address\n-------------------------------------------------")
+    for client in client_list:
+        print(client["ip"] + "\t\t\t" + client["mac"])
 
-user_options = get_arguments()
-scan_result = scan(user_options.ip)
-print_result(scan_result)
+options_list = parse_arguments()
+ip = options_list.ip
+client_list = arp_scan(ip)
+print_results(client_list)
